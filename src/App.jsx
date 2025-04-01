8d150063901780512f08dd15ee638bb8818e5970
aralston1281 <84287098+aralston1281@users.noreply.github.com>
2025-04-01 17:38:35 -0400

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>PDU Load Planner</h1>
-      <p>If you see this page, the app is wired up correctly 🎉</p>
-    </div>
-  );
-}
