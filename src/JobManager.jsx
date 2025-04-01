7d60681a98f88746923a7bd1542ae5cf8e72e432
aralston1281 <84287098+aralston1281@users.noreply.github.com>
2025-04-01 18:07:41 -0400

    Add files via upload

---
+++ src/JobManager.jsx
@@ -0,0 +1,8 @@
+
+export default function JobManager() {
+  return (
+    <div style={{ marginTop: 20, fontStyle: "italic", color: "#888" }}>
+      Job Manager component coming soon...
+    </div>
+  );
+}
