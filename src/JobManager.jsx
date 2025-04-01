f46ab2b89521ea080e2d24c49eb7482c3a931015
aralston1281 <84287098+aralston1281@users.noreply.github.com>
2025-04-01 18:18:59 -0400

    Delete src directory

--- src/JobManager.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function JobManager() {
-  return (
-    <div style={{ marginTop: 20, fontStyle: "italic", color: "#888" }}>
-      Job Manager component coming soon...
-    </div>
-  );
-}
